4654dfe20a30bfacb9d8590f749e6ae8a51f3b4c
Jatin Kamboj <jatin.kamboj@maropost.com>
2022-03-25 17:01:15 +0530

    Used activesupport gem as a dependency in foodie gem

--- spec/foodie_spec.rb
+++ spec/foodie_spec.rb
@@ -6,6 +6,10 @@ describe Foodie::Food do
     expect(Foodie::Food.portray("Broccoli")).to eql("Gross!")
   end
 
+  it "pluralises the brocollie" do
+    expect(Foodie::Food.pluralize("Broccoli")).to eql("Broccolis")
+  end
+
   it "anything else is delicious" do
     expect(Foodie::Food.portray("Not Broccoli")).to eql("Delicious!")
   end
